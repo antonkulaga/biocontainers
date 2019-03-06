3585391c0213ed25657fc5aaf01b3af2f6186282
antonkulaga <antonkulaga@gmail.com>
2019-03-06 19:08:57 +0200

    trying deseq dependency intallations again

--- R/diff-expression/DESeq2/install.R
+++ R/diff-expression/DESeq2/install.R
@@ -1,3 +1,2 @@
-library("Biocmanager")
 to_install <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "GEOmetadb", "SRAdb", "Biostrings", "BiocGenerics", "tximport")
 BiocManager::install(to_install)
