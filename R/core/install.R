cf8df10620cdbbd31423da7b8c83871de7387d0a
antonkulaga <antonkulaga@gmail.com>
2019-03-08 15:57:37 +0200

    update of dependencies

--- R/core/install.R
+++ R/core/install.R
@@ -2,5 +2,5 @@ if ("BiocInstaller" %in% rownames(installed.packages()))
 remove.packages("BiocInstaller")
 
 install.packages("BiocManager")
-to_install <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "GEOmetadb", "SRAdb", "Biostrings", "BiocGenerics", "AnnotationDbi", "tximport", "optparse")
+to_install <- c("Matrix", "KernSmooth", "mgcv", "devtools", "biomaRt", "GEOmetadb", "SRAdb", "Biostrings", "BiocGenerics", "AnnotationDbi", "tximport", "optparse", "ensembldb")
 BiocManager::install(to_install)
