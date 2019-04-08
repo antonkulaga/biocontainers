4fad3bc76d39ee5e34d99d2d3cb3b39aaa82a611
Anton Kulaga <antonkulaga@pop-os.localdomain>
2019-04-08 20:34:38 +0300

    minor update of Run.R

--- R/diff-expression/diff-express/src/run.R
+++ R/diff-expression/diff-express/src/run.R
@@ -1,39 +1,28 @@
 #!/usr/bin/env Rscript
-install.packages("argparse")
-library("argparse")
+library(optigrab)
+library(tximport)
 
-parser = ArgumentParser(prog='PROG')
-subparsers = parser$add_subparsers(help='sub-command help')
-to_genes = subparsers$add_parser('transcripts2genes', help='transcripts to genes')
-to_genes$add_argument('bar', type='integer', help='bar help')
+conversion = opt_get("transcripts2genes", default = "/data/indexes/gencode.vM20.metadata.EntrezGene")
+samples = opt_get("samples")
 
-# create the parser for the "b" command
-parser_b = subparsers$add_parser('b', help='b help')
-parser_b$add_argument('--baz', choices='XYZ', help='baz help')
+if(!is.na(samples)) {
+    print("")
+} else {
+    if(!is.na(conversion)) {
+        tx2gene = read.csv(file.path(dir, conversion), sep = "\t")
+        print("LOADING SAMPLES!")
+        print(opt$samples)
+        samples <- read.csv(samples, header = TRUE)
+        files <- file.path(samples$files)
+        tx2gene = read.csv(file.path(dir, "gencode.vM20.metadata.EntrezGene"), sep = "\t")
+        colnames(tx2gene) <- c("TXNAME","GENEID")
+        txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreAfterBar = TRUE)
+        ddsTxi <- DESeqDataSetFromTximport(txi, colData = samples, design = ~ 1)
+        dds <- DESeq(ddsTxi)
+        res <- results(dds)
+        print(res)
 
-option_list <- list(
-    make_option(c("-s", "--samples"), help="give .tsv files with samples",
-metavar="number")
-)
+    } else {
 
-# get command line options, if help option encountered print help and exit,
-# otherwise if options not found on command line then set defaults,
-opt <- parse_args(OptionParser(option_list=option_list))
-
-# print some progress messages to stderr if "quietly" wasn't requested
-if ( opt$samples == NULL) {
-    library(DESeq2)
-    library(tximport)
-    library(tximeta)
-    print("LOADING SAMPLES!")
-    print(opt$samples)
-    samples <- read.csv(samples, header = TRUE)
-    files <- file.path(samples$files)
-    tx2gene = read.csv(file.path(dir, "gencode.vM20.metadata.EntrezGene"), sep = "\t")
-    colnames(tx2gene) <- c("TXNAME","GENEID")
-    txi <- tximport(files, type = "salmon", tx2gene = tx2gene, ignoreAfterBar = TRUE)
-    ddsTxi <- DESeqDataSetFromTximport(txi, colData = samples, design = ~ 1)
-    dds <- DESeq(ddsTxi)
-    res <- results(dds)
-    print(res)
+    }
 }
