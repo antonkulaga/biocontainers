3896eac1949e999176a8fc45c1337f3319fc144f
Anton Kulaga <antonkulaga@gmail.com>
2020-09-24 03:54:44 +0300

    version updates of containers

--- R/core/install.R
+++ R/core/install.R
@@ -5,7 +5,8 @@ install.packages("BiocManager")
 to_install <- c(
     "Matrix", "KernSmooth", "mgcv", "Biostrings", "BiocGenerics", "S4Vectors",
     "devtools", "tximport", "tximeta", "optigrab", "optparse", #for development, counts import and CLI
-    "biomaRt","biomartr", "GEOmetadb", "SRAdb", "AnnotationDbi", "ensembldb", "AHEnsDbs" #for annotations
+    "biomaRt","biomartr", "GEOmetadb", "SRAdb", "AnnotationDbi", "ensembldb", "AHEnsDbs", #for annotations
+    "missForest", "doParallel"
 )
 BiocManager::install(to_install)
 #install.packages("biomartr", dependencies = TRUE)
