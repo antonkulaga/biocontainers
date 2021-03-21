86878f48607b7436d512dc3b91549c40059e0907
Anton Kulaga <antonkulaga@gmail.com>
2021-03-21 16:21:45 +0200

    updated methylation container

--- R/methylation/install.R
+++ R/methylation/install.R
@@ -1,2 +1,15 @@
-to_install <- c("tximport", "DeepBlueR", "DMRcaller", "LOLA", "MIRA","methylGSA","EnrichedHeatmap","epihet", "RankProd", "FlowSorted.Blood.450k", "minfi")
+to_install <- c("tximport",
+                "DeepBlueR",
+                "DMRcaller",
+                "LOLA",
+                "MIRA",
+                "methylGSA",
+                "EnrichedHeatmap",
+                "epihet",
+                "RankProd",
+                "FlowSorted.Blood.450k",
+                "minfi",
+                "MEDIPS",
+                "RUVSeq"
+)
 BiocManager::install(to_install)
